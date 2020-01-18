1f1d132cfa089baede3e79803c88a3370db4512c
Devendra Singh <devendra.616@gmail.com>
2020-01-19 01:17:49 +0530

    Updates

---
+++ precache-manifest.52a03c7c83f1f7930178c27cc7a49f42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00e9a207ac4783f28a28630f976fedda",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "61e3138c4b3025997e42",
+    "url": "/portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "a29b00e38a019d37225e",
+    "url": "/portfolio/static/js/2.5c9eea83.chunk.js"
+  },
+  {
+    "revision": "8de4bdbb127f280cc6b79279dd69f244",
+    "url": "/portfolio/static/js/2.5c9eea83.chunk.js.LICENSE"
+  },
+  {
+    "revision": "61e3138c4b3025997e42",
+    "url": "/portfolio/static/js/main.630ff7e9.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);
